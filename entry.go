2a2f95712859f82be2106fa788dbb53e0b7e66f2
Louis Chan <louischan0325@gmail.com>
2017-12-23 17:12:18 +0800

    Test entry

---
+++ entry.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"strings"
+)
+
+func getComment(c string) string {
+	return strings.TrimSpace(c[2 : len(c)-2])
+}
+
+func getStringValue(s string) string {
+	return s[1 : len(s)-1]
+}
+
+type entry struct {
+	comment string
+	key     string
+	value   string
+}
+
+func (ls entry) mergeCall(rc routineCall) entry {
+	ls.comment = rc.comment
+	if ls.comment == "" {
+		ls.comment = "No comment provided by engineer."
+	}
+	return ls
+}
+
+func (ls entry) mergeDev(dev entry) entry {
+	ls.comment = dev.comment
+	if ls.value == ls.key {
+		ls.value = dev.value
+	}
+	return ls
+}
+
+func (ls entry) print(suppressEmptyComment bool) string {
+	output := ""
+	printComment := !suppressEmptyComment || ls.comment != ""
+	if printComment {
+		output += "/* " + ls.comment + " */\n"
+	}
+	output += `"` + ls.key + `" = "` + ls.value + `";` + "\n\n"
+	return output
+}
---
+++ entry_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetComment(t *testing.T) {
+	rawComment := "/* comment */"
+	if getComment(rawComment) != "comment" {
+		t.Fail()
+	}
+}
+
+func TestGetStringValue(t *testing.T) {
+	stringLiteral := `"string"`
+	if getStringValue(stringLiteral) != "string" {
+		t.Fail()
+	}
+}
+
+func TestEntryMergeCall(t *testing.T) {
+	e := entry{}
+	call := routineCall{}
+	call.comment = "comment"
+
+	e = e.mergeCall(call)
+	if e.comment != call.comment {
+		t.Fail()
+	}
+
+	call.comment = ""
+	e = e.mergeCall(call)
+	if e.comment != "No comment provided by engineer." {
+		t.Fail()
+	}
+}
+
+func TestEntryMergeDev(t *testing.T) {
+	e := entry{}
+	dev := entry{
+		comment: "comment",
+	}
+	e = e.mergeDev(dev)
+	if e.comment != dev.comment {
+		t.Fail()
+	}
+
+	e = entry{
+		key:   "key",
+		value: "key",
+	}
+	dev = entry{
+		value: "value",
+	}
+	e = e.mergeDev(dev)
+	if e.value != dev.value {
+		t.Fail()
+	}
+}
+
+func TestEntryPrint(t *testing.T) {
+	e := entry{
+		comment: "comment",
+		key:     "key",
+		value:   "value",
+	}
+	out := `/* comment */
+"key" = "value";
+
+`
+	if e.print(false) != out {
+		t.Fail()
+	}
+
+	e = entry{
+		comment: "",
+		key:     "key",
+		value:   "value",
+	}
+	out = `"key" = "value";
+
+`
+	if e.print(true) != out {
+		t.Fail()
+	}
+}
