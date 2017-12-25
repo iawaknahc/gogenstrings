136c9c564ab78b248b2552056c7f0fcb5a415c1a
Louis Chan <louischan0325@gmail.com>
2017-12-25 18:20:53 +0800

    Fix default value of infoplist

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func parseOptionalRegexp(pattern string) (*regexp.Regexp, error) {
 
 func main() {
 	rootPtr := flag.String("root", ".", "the root path to the target")
-	infoPlistPtr := flag.String("infoplist", "<root>/Info.plist", "the path to Info.plist")
+	infoPlistPtr := flag.String("infoplist", "", "the path to Info.plist (default \"<root>/Info.plist\")")
 	devLangPtr := flag.String("devlang", "en", "the development language")
 	routinePtr := flag.String("routine", "NSLocalizedString", "the routine name to extract")
 	excludePtr := flag.String("exclude", "", "the regexp to exclude")
